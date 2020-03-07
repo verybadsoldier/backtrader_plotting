628453c32ca3d0b082b352197be4a5bc68ade4bc
verybadsoldier <vbs@springrts.de>
2020-03-07 14:55:00 +0100

    fix ticks not being rendered after old data has been rolled over

--- backtrader_plotting/bokeh/figureenvelope.py
+++ backtrader_plotting/bokeh/figureenvelope.py
@@ -258,18 +258,18 @@ class FigureEnvelope(object):
                 source=self._cds,
             ),
             code="""
-            // We override this axis' formatter's `doFormat` method
-            // with one that maps index ticks to dates. Some of those dates
-            // are undefined (e.g. those whose ticks fall out of defined data
-            // range) and we must filter out and account for those, otherwise
-            // the formatter computes invalid visible span and returns some
-            // labels as 'ERR'.
-            // Note, after this assignment statement, on next plot redrawing,
-            // our override `doFormat` will be called directly
-            // -- FunctionTickFormatter.doFormat(), i.e. _this_ code, no longer
-            // executes.  
+                // We override this axis' formatter's `doFormat` method
+                // with one that maps index ticks to dates. Some of those dates
+                // are undefined (e.g. those whose ticks fall out of defined data
+                // range) and we must filter out and account for those, otherwise
+                // the formatter computes invalid visible span and returns some
+                // labels as 'ERR'.
+                // Note, after this assignment statement, on next plot redrawing,
+                // our override `doFormat` will be called directly
+                // -- FunctionTickFormatter.doFormat(), i.e. _this_ code, no longer
+                // executes.
                 axis.formatter.doFormat = function (ticks) {
-                    const dates = ticks.map(i => source.data.datetime[i]),
+                    const dates = ticks.map(i => source.data.datetime[source.data.index.indexOf(i)]),
                           valid = t => t !== undefined,
                           labels = formatter.doFormat(dates.filter(valid));
                     let i = 0;

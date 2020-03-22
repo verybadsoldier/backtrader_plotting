01e1d8ed90604cb7a5ed84c57fdfbb3a8ceeb872
verybadsoldier <vbs@springrts.de>
2020-03-22 17:29:02 +0100

    1.1.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name='backtrader_plotting',
 
-    version='1.0.0',
+    version='1.1.0',
 
     description='Plotting package for Backtrader (Bokeh)',
 

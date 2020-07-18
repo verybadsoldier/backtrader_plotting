a3d0ff272dc8109e8729819e7b6adce98143c9ba
verybadsoldier <vbs@springrts.de>
2020-07-18 15:32:01 +0200

    disabled analyzer AnnualReturn for live mode

--- backtrader_plotting/analyzer_tables/annualreturn.py
+++ backtrader_plotting/analyzer_tables/annualreturn.py
@@ -4,11 +4,15 @@ from backtrader_plotting.bokeh.datatable import ColummDataType
 def datatable(self):
     cols1 = [['Year', ColummDataType.STRING], ['Return', ColummDataType.FLOAT]]
 
-    a = self.get_analysis()
+    if getattr(self, 'ret', None) is None:
+        # analyzer is only filled on 'stop'. So empty in live mode
+        cols1[0].append('No data available. Live mode?')
+        cols1[1].append('0.0')
+    else:
+        a = self.get_analysis()
 
-    for k, v in a.items():
-        cols1[0].append(k)
-        cols1[1].append(v)
+        for k, v in a.items():
+            cols1[0].append(k)
+            cols1[1].append(v)
 
     return "Annual Return", [cols1]
-
